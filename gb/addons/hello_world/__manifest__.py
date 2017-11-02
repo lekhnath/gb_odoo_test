0844ace9d35baf4496b4b552790dd59b5f0139ad
Lekhnath Rijal <049lekhnath@gmail.com>
2017-11-02 13:38:18 +0545

    Initial Commit

---
+++ gb/addons/hello_world/__manifest__.py
@@ -0,0 +1,10 @@
+{
+  'name': 'Hello World',
+  'description': 'Hello world odoo addon',
+  'version': '0.0.1',
+  'author': 'Lekhnath Rijal',
+  'category': 'Uncategorized',
+  'application': True,
+  'installable': True,
+  'depends': ['base'],
+}
